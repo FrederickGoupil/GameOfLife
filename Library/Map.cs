87f9b8bd3f39c3edb3b89f87992612aa6a405d60
FrederickGoupil <157034072+Victo-2302008@users.noreply.github.com>
2024-02-15 16:53:37 -0500

    uuuh

---
+++ Library/Map.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Map
+    {
+        private int sizeX;
+        private int sizeY;
+        private Square[] squares;
+
+        public Map(int sizeX, int sizeY)
+        {
+            this.sizeX = sizeX;
+            this.sizeY = sizeY;
+
+            this.squares = new Square[sizeX * sizeY];
+
+            for (int x = 0; x < sizeX; x++)
+            {
+                for (int y = 0; y < sizeY; y++)
+                {
+                    squares[i] = new Square(x, y, '#');
+                }
+            }
+        }
+        public void RenderMap()
+        {
+            Console.Clear();
+
+            for (int y = 0; y < sizeY;y++)
+            {
+                for(int x = 0;x < sizeX;x++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+        }   
+    }
+}
---
+++ Library/Square.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Square
+    {
+        private char content;
+        private int positionX;
+        private int positionY;
+
+        public Square(int  positionX, int positionY, char content)
+        {
+            this.positionX = positionX;
+            this.positionY = positionY;
+            this.content = content;
+        }
+    }
+}
